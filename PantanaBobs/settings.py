68bb26e75d03a4ff12a4a53b7f0e0d1d2bf712a8
The-Noble-K <jarod.sneed@gmail.com>
2019-07-25 15:33:04 -0400

    bootstrappin

--- PantanaBobs/settings.py
+++ PantanaBobs/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'PantanaBobs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["PantanaBobs/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
