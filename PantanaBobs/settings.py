948c524295ef061b5c7ab844b280a0372b700542
The-Noble-K <jarod.sneed@gmail.com>
2019-07-26 19:58:47 -0400

    removed static

--- PantanaBobs/settings.py
+++ PantanaBobs/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'index'
 ]
 
@@ -118,5 +117,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_ROOT = '/static/'
+
 
